a00311deba9e5105e094126b8d3bbc5780e6dcd4
Ning Xuan <wee_ning_xuan_2028@mgs.sch.edu.sg>
2025-08-02 09:53:44 +0800

    Initial Commit

---
+++ stickman/stickmanApp.swift
@@ -0,0 +1,17 @@
+//
+//  stickmanApp.swift
+//  stickman
+//
+//  Created by T Krobot on 2/8/25.
+//
+
+import SwiftUI
+
+@main
+struct stickmanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
